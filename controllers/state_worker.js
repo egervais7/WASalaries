1caefd20a4675066ef8a1a3a2ec8903d36ee3390
egervais7 <edward.a.gervais89@gmail.com>
2016-01-26 10:19:11 -0800

    added route in the state_worker controller to find all in model

--- controllers/state_worker.js
+++ controllers/state_worker.js
@@ -3,6 +3,13 @@ var Worker = require('../models/state_worker');
 
 var router = express.Router();
 
-
+router.route('/')
+  .get(function(req, res){
+    Worker.find(function(error, workers){
+      if (error) return;
+      res.status(500).send(error);
+      res.send(workers);
+    });
+  });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/api/state_worker',
   require('./controllers/state_worker'));
 
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(3000);
