7f98be38658247762d432b594350398e31c14f3a
jamesjchi <jamesjchi@gmail.com>
2016-01-27 10:35:58 -0800

    added sum/pagination

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -24,7 +24,28 @@ $scope.search = function(){
 
 };
 
+count = 2500;
+    var data = [];  
+    while(count) {
+      data[count] = count--;
+    } 
+    $scope.data = data;
+  
+    $scope.currentPage = 0;
+    $scope.pageSize = 50;
+  
+  
+    $scope.numberOfPages=function(){
+        return Math.floor($scope.data.length/$scope.pageSize);                
+    };
+
 }])
 .controller('SalaryCtrl', ['$scope', '$routeParams', 'Salary', function($scope, $routeParams, Salary){
 
-}]);
+}])
+.filter('startFrom', function() {
+    return function(input, start) {
+        start = +start; 
+        return input.slice(start);
+    };
+});
